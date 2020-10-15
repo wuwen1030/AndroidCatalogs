de28e044d8c97ba6b8acc7d123b284a76e5b0caf
wuwen <wuwen.xb@alibaba-inc.com>
2020-10-15 15:22:25 +0800

    fix: fix typo

--- app/src/main/java/com/wuwen/catalog/CounterActivity.kt
+++ app/src/main/java/com/wuwen/catalog/CounterActivity.kt
@@ -7,6 +7,6 @@ class CounterActivity : WithBackActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_counter_main)
-        supportActionBar?.title = "View model Sample"
+        supportActionBar?.title = "View Model Sample"
     }
 }
